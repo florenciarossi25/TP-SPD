5de6e892dc24d1c5c343d0e46ad5f4579f7d9218
florenciarossi25 <41929711+florenciarossi25@users.noreply.github.com>
2022-11-15 15:25:20 -0300

    Add files via upload

--- LIBTP.asm
+++ LIBTP.asm
@@ -2,13 +2,21 @@
 .model small
 .stack 100h
 .data
-nota	db "Ingrese la nota:",0dh, 0ah
-		db "1. DO", 0dh, 0ah, "2. RE", 0dh, 0ah, "3. MI", 0dh, 0ah, "4. FA", 0dh, 0ah, "5. SOL", 0dh, 0ah, "6. LA", 0dh, 0ah, "7. SI", 0dh, 0ah, 24h 
+;nota	db "Ingrese la nota:",0dh, 0ah
+;		db "1. DO", 0dh, 0ah, "2. RE", 0dh, 0ah, "3. MI", 0dh, 0ah, "4. FA", 0dh, 0ah, "5. SOL", 0dh, 0ah, "6. LA", 0dh, 0ah, "7. SI", 0dh, 0ah, 24h 
+salto	db 0dh, 0ah, 24h
+errorN	db "Ingrese una nota v", 160, "lida", 0dh, 0ah, 24h
+sePaso	db "Se ha exedido!!! Porfavor ingrese las notas nuevamente", 0dh, 0ah, 24h
+nota	db "Ingrese la nota (D=do, R=re, M=mi, F=fa, S=sol, L=la, I=si", 0dh, 0ah, 24h
+tiempo	db "Ingrese el tempo (Redonda=x,blanca=b,negra=n,corchea=c)", 0dh, 0ah, 24h
 .code
 
 public mayus
-public ingresa
+public sampler
+public samplercarga
+public play
 
+;MAYUS-----------------------------------------------------------------------------------------------------------------
 mayus proc
 ;Esta función retorna la letra en AL mayusculizada
 
@@ -53,80 +61,302 @@ finMayus:
 ret 
 mayus endp
 
-ingresa proc
-;Recibe por STACK el offset de la variable dos variables (notas y tiempo) y las rellena con los valores correspondientes
+;SAMPLER---------------------------------------------------------------------------------------------------------------
+
+sampler proc
 	push bp
-	mov bp, sp
+    mov bp,sp
+    push dx
+    push cx
+    push bx
+  	mov di, ss:[bp+6] ;offstet compas frecuencias
+  	mov si, ss:[bp+4] ;offset compas tempos
 
-	mov bx, ss:[bp+6]							;OFFSET NOTAS
-	mov cx, ss:[bp+4]							;OFFSET TIEMPO
+ leefrecuencia:
+ 	cmp byte ptr [di],24h
+ 	je finproc1
+ 	cmp byte ptr [si],24h
+ 	je finproc1
+ 	cmp byte ptr [di],65h
+ 	je do
+ 	cmp byte ptr [di],72h
+ 	je re
+ 	cmp byte ptr [di],74h
+ 	je mi
+ 	cmp byte ptr [di],79h
+ 	je fa
+ 	cmp byte ptr [di],75h
+ 	je sol
+ 	cmp byte ptr [di],69h
+ 	je la2
+ 	cmp byte ptr [di],6fh
+ 	je s2
+ do: 
+ 	mov cx, 4560
+ 	cmp byte ptr [si],78h ;x
+	je redonda2
+	cmp byte ptr [si],62h ;b
+	je blanca2
+	cmp byte ptr [si],6eh ;n
+	je negra2
+	cmp byte ptr [si],63h ;c
+	je corchea2
+re: 
+	mov cx, 4063
+	cmp byte ptr [si],78h ;x
+	je redonda2
+	cmp byte ptr [si],62h ;b
+	je blanca2
+	cmp byte ptr [si],6eh ;n
+	je negra2
+	cmp byte ptr [si],63h ;c
+	je corchea2
+mi: 
+	mov cx, 3619
+	cmp byte ptr [si],78h ;x
+	je redonda2
+	cmp byte ptr [si],62h ;b
+	je blanca2
+	cmp byte ptr [si],6eh ;n
+	je negra2
+	cmp byte ptr [si],63h ;c
+	je corchea2
 
-mov cx, 4
-notas:
-	mov ah, 0Fh
+;saltos auxiliares 
+s2: 
+jmp s1
+redonda2:
+jmp redonda
+finproc1: 
+jmp finproc
+blanca2:
+jmp blanca
+negra2:
+jmp negra
+corchea2:
+jmp corchea
+la2:
+jmp la
+fa: mov cx, 3416
+	cmp byte ptr [si],78h ;x
+	je redonda
+	cmp byte ptr [si],62h ;b
+	je blanca
+	cmp byte ptr [si],6eh ;n
+	je negra
+	cmp byte ptr [si],63h ;c
+	je corchea
+sol:mov cx, 3043
+	cmp byte ptr [si],78h ;x
+	je redonda
+	cmp byte ptr [si],62h ;b
+	je blanca
+	cmp byte ptr [si],6eh ;n
+	je negra
+	cmp byte ptr [si],63h ;c
+	je corchea
+la: mov cx, 2711
+	cmp byte ptr [si],78h ;x
+	je redonda
+	cmp byte ptr [si],62h ;b
+	je blanca
+	cmp byte ptr [si],6eh ;n
+	je negra
+	cmp byte ptr [si],63h ;c
+	je corchea
+s1:	mov cx, 2415
+	cmp byte ptr [si],78h ;x
+	je redonda
+	cmp byte ptr [si],62h ;b
+	je blanca
+	cmp byte ptr [si],6eh ;n
+	je negra
+	cmp byte ptr [si],63h ;c
+	je corchea
+redonda:
+ mov bx,500
+ call play
+ inc di
+ inc si
+ xor cx,cx
+ xor bx,bx
+ jmp leefrecuencia
+blanca:
+mov bx, 250
+call play
+ inc di
+ inc si
+ xor cx,cx
+ xor bx,bx
+ jmp leefrecuencia
+negra:
+mov bx, 125
+call play
+ inc di
+ inc si
+ xor cx,cx
+ xor bx,bx
+ jmp leefrecuencia
+corchea:
+mov bx,75
+call play
+ inc di
+ inc si
+ xor cx,cx
+ xor bx,bx
+ jmp leefrecuencia
+
+finproc:
+pop bp
+pop dx
+pop cx
+pop bx
+ret 4
+sampler endp
+
+;SAMPLERCARGA----------------------------------------------------------------------------------------------------------
+proc samplercarga
+	push bp
+    mov bp,sp
+    push ax
+    push cx
+    push bx
+
+  	mov bx, ss:[bp+6] ;offstet compas frecuencias
+  	mov si, ss:[bp+4] ;offset compas tempos
+
+inicio:
+    mov cx,0
+frecuencia: 
+	
+	mov ah, 9
+	lea dx, nota
+	int 21h
+
+    mov ah, 1 
+    int 21h 
+    mov byte ptr[bx],al
+    inc bx
+tempo:
+	mov ah, 0Fh				;LIMPIA LA PANTALLA
 	int 10h
 	mov ah, 0
 	int 10h
 
 	mov ah, 9
-	lea dx, nota
+	lea dx, tiempo
 	int 21h
 
-	mov ah, 1
+	mov ah,1 				; redondaX=4,blancaB=2,negraN=1,corcheaC 0.5 compas=4 
 	int 21h
 
-	cmp al, 1
-	je esDo
-	cmp al, 2
-	je esRe
-	cmp al, 3
-	je esMi
-	cmp al, 4
-	je esFa
-	cmp al, 5
-	je esSol
-	cmp al, 6
-	je esLa
-	cmp al, 7
-	je esSi
+	cmp al,78h ;x
+	je esRedonda
+	cmp al,62h ;b
+	je esBlanca
+	cmp al,6eh ;n
+	je esNegra
+	cmp al,63h ;c
+	je esCorchea
+
+	mov ah, 9
+	lea dx, errorN
+	int 21h 
+	jmp tempo
+SaveTempo:
+	mov byte ptr[si],al
+	inc si
+	cmp cx,4
+	je finCarga
+	jmp frecuencia
+esRedonda:
+	add cx,4
+	cmp cx,4
+	ja borro
+	
+	jmp SaveTempo
+esBlanca:
+	add cx,2
+	cmp cx,4
+	ja borro
+	
+	jmp SaveTempo
+esNegra:
+	add cx,1
+	cmp cx,4
+	ja borro
+
+	jmp SaveTempo
+esCorchea:
+	add cx, 1
+	cmp cx,4
+	ja borro
+	
+	jmp SaveTempo
+
+	
+borro:
+	mov ah, 0Fh				;LIMPIA LA PANTALLA
+	int 10h
+	mov ah, 0
+	int 10h
+
+	mov ah, 9
+	lea dx, sePaso
+	int 21h
+	mov bx, ss:[bp+6] ;offstet compas frecuencias
+  	mov si, ss:[bp+4] ;offset compas tempos
+  	mov cx, 8
+borro2:	
+	mov byte ptr[bx],24h
+	mov byte ptr[si],24h
+	loop borro2
+	jmp inicio
 
 
-esDo:
-	mov [bx], 4560
-	inc bx
-	jmp tiempo
-esRe:
-	mov [bx], 4063
-	inc bx
-	jmp tiempo
-esMi:
-	mov [bx], 3619
-	inc bx
-	jmp tiempo
-esFa:
-	mov [bx], 3416
-	inc bx
-	jmp tiempo
-esSol:
-	mov [bx], 3043
-	inc bx
-	jmp tiempo
-esLa:
-	mov [bx], 2711
-	inc bx
-	jmp tiempo
-esSi:
-	mov [bx], 2415
-	inc bx
-	jmp tiempo
-
-
-tiempo:
-loop notas
-
-pop bp	
-
+finCarga:
+pop bx
+pop cx
+pop ax
+pop bp
 ret 4
-ingresa endp
+samplercarga endp
+
+;PLAY------------------------------------------------------------------------------------------------------------------
+play proc
+
+play:
+    push ax
+    push cx
+    push bx
+    mov     ax, cx
+
+    out     42h, al
+    mov     al, ah
+    out     42h, al
+    in      al, 61h
+
+    or      al, 00000011b
+    out     61h, al
+
+    pause1:
+        mov cx, 65535
+
+    pause2:
+        dec cx
+        jne pause2
+        dec bx
+        jne pause1
+
+        in  al, 61h
+        and al, 11111100b
+        out 61h, al
+        
+    pop bx
+    pop cx
+    pop ax
+
+    ret
+play endp
 
 end
